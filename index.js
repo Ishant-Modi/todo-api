ff95320c543bd0ff2a6b198ffb177ea78ffe1ad3
Ishant <ishant.mdi6@gmail.com>
2026-08-17 13:48:16 +0530

    Stage 5: Swagger UI

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const openapiSpec = require('./openapi.json');
+
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 let tasks = [
   { id: 1, title: "Buy milk", done: false },
